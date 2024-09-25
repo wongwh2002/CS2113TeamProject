312ccd340a2bf122e793221b1d07d6fcd66ed1f4
TPH777 <pinghuitan80@gmail.com>
2024-09-26 03:04:17 +0800

    Add income and spending class
    By having individual classes, more informations can be added in the
    future versions such as description and date

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,21 +1,32 @@
 package seedu.duke;
 
+import type.Income;
+import type.Spending;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    /**
-     * Main entry-point for the java.duke.Duke application.
-     */
+
+    private static final ArrayList<Income> incomes = new ArrayList<Income>();
+    private static final ArrayList<Spending> spendings = new ArrayList<Spending>();
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from Wiagi\n");
         System.out.println("What is your name?");
 
         Scanner in = new Scanner(System.in);
         System.out.println("Hello " + in.nextLine());
+
+        /* Test code to print input in array list
+        Income testIncome = new Income(100);
+        incomes.add(testIncome);
+
+        Spending testSpending = new Spending(100);
+        spendings.add(testSpending);
+
+        System.out.println(incomes.get(0).amount);
+        System.out.println(spendings.get(0).amount);
+        */
     }
 }
