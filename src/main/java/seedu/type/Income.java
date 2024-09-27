ef66840d4ece15b65e25d90bc979f1be059b767a
NigelYeoTW <nigelyeotongwei@gmail.com>
2024-09-27 18:42:49 +0800

    Add entry function for tracker
    Currently, the tracker does not allow users to track input.
    
    A add entry function is created to allow users to track their incomes as
    well as spendings. The function call is ivoked with 'add' keyword. The
    format is of add[add type][amount][description]. Add type can be of
    'income' or 'spending' types right now.

--- src/main/java/seedu/type/Income.java
+++ src/main/java/seedu/type/Income.java
@@ -1,9 +1,30 @@
 package seedu.type;
 
-public class Income {
-    public int amount;
+import seedu.exception.WiagiEmptyDescriptionException;
 
-    public Income(int amount) {
-        this.amount = amount;
+public class Income {
+    private int incomeAmount;
+    private String incomeDescription;
+
+    private String getIncomeDescription(int incomeAmount, String userInput) throws WiagiEmptyDescriptionException {
+        String[] incomeDescriptionWords = userInput.split(Integer.toString(incomeAmount));
+        if (incomeDescriptionWords.length == 1) {
+            throw new WiagiEmptyDescriptionException();
+        }
+        return incomeDescriptionWords[1].trim();
+    }
+
+    public Income(String[] userInputWords, String userInput) {
+        try {
+            int incomeAmount = Integer.parseInt(userInputWords[2]);
+            String incomeDescription = getIncomeDescription(incomeAmount, userInput);
+            this.incomeAmount = incomeAmount;
+            this.incomeDescription = incomeDescription;
+            System.out.println("Entry successfully added!");
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Did not enter a valid income amount!");
+        } catch (WiagiEmptyDescriptionException e) {
+            System.out.println(e);
+        }
     }
 }
