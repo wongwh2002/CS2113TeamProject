5c846d420c62043a59728391823f2d42a43dab62
NigelYeoTW <nigelyeotongwei@gmail.com>
2024-10-15 22:31:36 +0800

    Fix bug with LoginStorageTest

--- src/test/java/seedu/storage/LoginStorageTest.java
+++ src/test/java/seedu/storage/LoginStorageTest.java
@@ -8,7 +8,7 @@ import java.io.ByteArrayInputStream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginStorageTest {
-    void provideInput(String data) {
+    private void provideInput(String data) {
         ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
         System.setIn(testIn);
     }
@@ -17,8 +17,8 @@ public class LoginStorageTest {
     void loginStorage_userPassword_correctHashCode() {
         String userPassword = "password";
         int hashCode = userPassword.hashCode();
-        Ui ui = new Ui();
         provideInput("password");
+        Ui ui = new Ui();
         LoginStorage.load(ui);
         assertEquals(hashCode, Storage.password);
     }
