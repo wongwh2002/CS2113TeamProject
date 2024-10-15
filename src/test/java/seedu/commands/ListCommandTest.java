abeb9d0331b4019210567ac7bdeedd29ff01e28b
wongwh2002 <wenghongwong5686@gmail.com>
2024-10-15 18:20:53 +0800

    edited based on junittest

--- src/test/java/seedu/commands/ListCommandTest.java
+++ src/test/java/seedu/commands/ListCommandTest.java
@@ -1,15 +1,15 @@
 package seedu.commands;
 
-import seedu.type.IncomeList;
-import seedu.type.SpendingList;
+//import seedu.type.IncomeList;
+//import seedu.type.SpendingList;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ListCommandTest {
 
