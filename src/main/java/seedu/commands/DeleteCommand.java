055070aa33e7dcad8bd8923a2e4a7ce1a5b6dff2
wx-03 <tngwenxi@gmail.com>
2024-10-15 21:40:20 +0800

    Use generics to extract delete method for income and spending

--- src/main/java/seedu/commands/DeleteCommand.java
+++ src/main/java/seedu/commands/DeleteCommand.java
@@ -22,20 +22,10 @@ public class DeleteCommand extends Command {
                         "Please enter in the form: delete [spending/income] [index]");
             }
 
-            if (fullCommandArray[1].equalsIgnoreCase("income")) {
-                int idx = getIdx(fullCommandArray);
-                if (isOutOfBounds(idx, incomes)) {
-                    throw new IllegalArgumentException("Invalid index");
-                }
-                incomes.remove(incomes.get(idx));
-                Ui.printWithTab("Successfully deleted!");
-            } else if (fullCommandArray[1].equalsIgnoreCase("spending")) {
-                int idx = getIdx(fullCommandArray);
-                if (isOutOfBounds(idx, spendings)) {
-                    throw new IllegalArgumentException("Invalid index");
-                }
-                spendings.remove(spendings.get(idx));
-                Ui.printWithTab("Successfully deleted!");
+            if (userInputWords[1].equalsIgnoreCase("income")) {
+                deleteEntry(userInputWords, incomes);
+            } else if (userInputWords[1].equalsIgnoreCase("spending")) {
+                deleteEntry(userInputWords, spendings);
             } else {
                 throw new IllegalArgumentException("Invalid input. " +
                         "Please enter in the form: delete [spending/income] [index]");
@@ -47,6 +37,15 @@ public class DeleteCommand extends Command {
 
     private <T> boolean isOutOfBounds(int index, ArrayList<T> arrList) {
         return (index >= arrList.size() || index < 0);
+    }
+
+    private <T> void deleteEntry(String[] userInputWords, ArrayList<T> arrList) {
+        int idx = getIdx(userInputWords);
+        if (isOutOfBounds(idx, arrList)) {
+            throw new IllegalArgumentException("Invalid index");
+        }
+        arrList.remove(arrList.get(idx));
+        Ui.printWithTab("Successfully deleted!");
     }
 
     private int getIdx(String[] fullCommandArray) {
