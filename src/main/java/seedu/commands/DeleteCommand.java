ab8eaa5b3a0669a2baae8427d4f00539a3c49a31
TPH777 <pinghuitan80@gmail.com>
2024-10-27 06:56:11 +0800

    Match DeleteCommand to EditCommand style

--- src/main/java/seedu/commands/DeleteCommand.java
+++ src/main/java/seedu/commands/DeleteCommand.java
@@ -15,10 +15,15 @@ import static seedu.classes.Constants.INDEX_NOT_INTEGER;
 import static seedu.classes.Constants.INDEX_OUT_OF_BOUNDS;
 import static seedu.classes.Constants.INVALID_CATEGORY;
 import static seedu.classes.Constants.SPACE_REGEX;
+import static seedu.classes.Constants.INCOME;
+import static seedu.classes.Constants.SPENDING;
 
 public class DeleteCommand extends Command {
 
     public static final String COMMAND_WORD = "delete";
+    private static final int TYPE_INDEX = 1;
+    private static final int INDEX_OF_ENTRY_INDEX = 2;
+    private static final int DELETE_COMPULSORY_ARGUMENTS_LENGTH = 3;
 
     private final String fullCommand;
     public DeleteCommand(String fullCommand) {
@@ -28,44 +33,53 @@ public class DeleteCommand extends Command {
     // @@author wx-03
     @Override
     public void execute(IncomeList incomes, SpendingList spendings) {
-        String[] arguments = fullCommand.split(SPACE_REGEX);
+        assert incomes != null;
+        assert spendings != null;
         try {
-            if (arguments.length < 3) {
-                throw new WiagiMissingParamsException(INCORRECT_PARAMS_NUMBER
-                        + DELETE_COMMAND_FORMAT);
-            }
-            if (arguments[1].equalsIgnoreCase("income")) {
-                deleteEntry(arguments, incomes);
-            } else if (arguments[1].equalsIgnoreCase("spending")) {
-                deleteEntry(arguments, spendings);
-            } else {
-                throw new WiagiInvalidInputException(INVALID_CATEGORY + DELETE_COMMAND_FORMAT);
-            }
+            commandHandler(incomes, spendings);
         } catch (WiagiMissingParamsException | WiagiInvalidInputException | WiagiInvalidIndexException e) {
             Ui.printWithTab(e.getMessage());
         }
     }
 
-    private <T> boolean isOutOfBounds(int index, ArrayList<T> arrList) {
-        return (index >= arrList.size() || index < 0);
+    private void commandHandler(IncomeList incomes, SpendingList spendings)
+            throws WiagiMissingParamsException, WiagiInvalidIndexException {
+        String[] arguments = extractArguments();
+        String type = arguments[TYPE_INDEX];
+        if (!(type.equals(SPENDING) || type.equals(INCOME))) {
+            throw new WiagiInvalidInputException(INVALID_CATEGORY + DELETE_COMMAND_FORMAT);
+        }
+        switch (type) {
+        case INCOME:
+            deleteEntry(arguments, incomes);
+            break;
+        case SPENDING:
+            deleteEntry(arguments, spendings);
+            break;
+        default:
+            throw new WiagiInvalidInputException(INVALID_CATEGORY + DELETE_COMMAND_FORMAT);
+        }
+    }
+
+    private String[] extractArguments() throws WiagiMissingParamsException {
+        String[] arguments = fullCommand.split(SPACE_REGEX, DELETE_COMPULSORY_ARGUMENTS_LENGTH);
+        if (arguments.length < DELETE_COMPULSORY_ARGUMENTS_LENGTH) {
+            throw new WiagiMissingParamsException(INCORRECT_PARAMS_NUMBER
+                    + DELETE_COMMAND_FORMAT);
+        }
+        return arguments;
     }
 
     private <T> void deleteEntry(String[] arguments, ArrayList<T> arrList) throws WiagiInvalidIndexException {
-        int index = getIndex(arguments);
-        if (isOutOfBounds(index, arrList)) {
-            throw new WiagiInvalidIndexException(INDEX_OUT_OF_BOUNDS);
-        }
-        assert arrList.get(index) != null : "entry to delete cannot be null";
-        arrList.remove(arrList.get(index));
-        Ui.printWithTab("Successfully deleted!");
-    }
-
-    private int getIndex(String[] fullCommandArray) {
+        String stringIndex = arguments[INDEX_OF_ENTRY_INDEX];
         try {
-            int index = Integer.parseInt(fullCommandArray[2]);
-            return index - 1;
+            int index = Integer.parseInt(stringIndex) - 1;
+            arrList.remove(index);
         } catch (NumberFormatException e) {
             throw new WiagiInvalidInputException(INDEX_NOT_INTEGER + DELETE_COMMAND_FORMAT);
+        } catch (IndexOutOfBoundsException e) {
+            throw new WiagiInvalidIndexException(INDEX_OUT_OF_BOUNDS);
         }
+        Ui.printWithTab("Successfully deleted!");
     }
 }
