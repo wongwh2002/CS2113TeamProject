bdfbc3c821aa8b18d5530deeba3a2d0a34beb320
wongwh2002 <wenghongwong5686@gmail.com>
2024-11-09 21:38:37 +0800

    LoadStorageCheck test

--- src/test/java/seedu/storage/IncomeListStorageTest.java
+++ src/test/java/seedu/storage/IncomeListStorageTest.java
@@ -65,9 +65,11 @@ public class IncomeListStorageTest {
         if (incomeFile.exists()) {
             incomeFile.delete();
         }
-        try (FileWriter writer = new FileWriter(incomeFile)) {
+        try {
+            FileWriter writer = new FileWriter(incomeFile);
             writer.write("10.0|savings|2024-11-09||NONE|null|1\n");
             writer.write("20.0|investment|2024-11-09||MONTHLY|2024-11-09|15\n");
+            writer.close();
         } catch (IOException e) {
             Ui.printWithTab("load_multipleLinesfromFile_success() fails due to fileWriter IOException");
         }
--- src/test/java/seedu/storage/LoadStorageCheckTest.java
+++ src/test/java/seedu/storage/LoadStorageCheckTest.java
@@ -1,32 +1,19 @@
 package seedu.storage;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
-import seedu.classes.Ui;
 import seedu.exception.WiagiStorageCorruptedException;
 import seedu.recurrence.RecurrenceFrequency;
 import seedu.type.Income;
-import seedu.type.IncomeList;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import seedu.type.Spending;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.classes.Constants.VALID_TEST_DATE;
 
 public class LoadStorageCheckTest {
 
     @Test
-    public void parseEntry_validInput_success() {
+    public void parseEntry_validIncomeInput_success() {
         LoadStorageCheck storageUtils = new LoadStorageCheck("income");
         String newEntry = "10.0|savings|2024-11-09||NONE|null|1";
         Income income = (Income) storageUtils.parseEntry(newEntry);
@@ -37,6 +24,20 @@ public class LoadStorageCheckTest {
         assertEquals(RecurrenceFrequency.NONE, income.getRecurrenceFrequency());
         assertNull(income.getLastRecurrence());
         assertEquals(1, income.getDayOfRecurrence());
+    }
+
+    @Test
+    public void parseEntry_validSpendingInput_success() {
+        LoadStorageCheck storageUtils = new LoadStorageCheck("spending");
+        String newEntry = "10.0|kfc|2024-11-09||NONE|null|1";
+        Spending spending = (Spending) storageUtils.parseEntry(newEntry);
+        assertEquals(10.0, spending.getAmount());
+        assertEquals("kfc", spending.getDescription());
+        assertEquals(VALID_TEST_DATE, spending.getDate());
+        assertEquals("", spending.getTag());
+        assertEquals(RecurrenceFrequency.NONE, spending.getRecurrenceFrequency());
+        assertNull(spending.getLastRecurrence());
+        assertEquals(1, spending.getDayOfRecurrence());
     }
 
     @Test
@@ -156,7 +157,8 @@ public class LoadStorageCheckTest {
         try {
             storageUtils.parseEntry(newEntry);
         } catch (WiagiStorageCorruptedException e) {
-            assertEquals(storageUtils.storageErrorMessage + LoadStorageCheck.STORAGE_RECURRENCE_FREQUENCY, e.getMessage());
+            assertEquals(storageUtils.storageErrorMessage + LoadStorageCheck.STORAGE_RECURRENCE_FREQUENCY,
+                    e.getMessage());
         }
     }
 
@@ -182,16 +184,47 @@ public class LoadStorageCheckTest {
         }
     }
 
-
-
-
-
-
-    @BeforeEach
-    void setUp() {
+    @Test
+    public void parseEntry_invalidLastRecurredDate_storageCorruptedExceptionthrown() {
+        LoadStorageCheck storageUtils = new LoadStorageCheck("income");
+        String newEntry = "10.0|savings|2024-11-09||DAILY|null|1";
+        try {
+            storageUtils.parseEntry(newEntry);
+        } catch (WiagiStorageCorruptedException e) {
+            assertEquals(storageUtils.storageErrorMessage + LoadStorageCheck.STORAGE_LAST_RECURRED_DATE, e.getMessage());
+        }
     }
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    public void parseEntry_nullDayOfRecurrence_storageCorruptedExceptionthrown() {
+        LoadStorageCheck storageUtils = new LoadStorageCheck("income");
+        String newEntry = "10.0|savings|2024-11-09||NONE|null|null";
+        try {
+            storageUtils.parseEntry(newEntry);
+        } catch (WiagiStorageCorruptedException e) {
+            assertEquals(storageUtils.storageErrorMessage + LoadStorageCheck.STORAGE_DAY_RECURRENCE, e.getMessage());
+        }
+    }
+
+    @Test
+    public void parseEntry_tooLargeDayOfRecurrence_storageCorruptedExceptionthrown() {
+        LoadStorageCheck storageUtils = new LoadStorageCheck("income");
+        String newEntry = "10.0|savings|2024-11-09||NONE|null|32";
+        try {
+            storageUtils.parseEntry(newEntry);
+        } catch (WiagiStorageCorruptedException e) {
+            assertEquals(storageUtils.storageErrorMessage + LoadStorageCheck.STORAGE_VALID_DAY_RECURRENCE, e.getMessage());
+        }
+    }
+
+    @Test
+    public void parseEntry_tooSmallDayOfRecurrence_storageCorruptedExceptionthrown() {
+        LoadStorageCheck storageUtils = new LoadStorageCheck("income");
+        String newEntry = "10.0|savings|2024-11-09||NONE|null|0";
+        try {
+            storageUtils.parseEntry(newEntry);
+        } catch (WiagiStorageCorruptedException e) {
+            assertEquals(storageUtils.storageErrorMessage + LoadStorageCheck.STORAGE_VALID_DAY_RECURRENCE, e.getMessage());
+        }
     }
 }
