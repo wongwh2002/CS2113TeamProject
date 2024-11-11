4d419bf73502b699507482a00f053033cc91a7b8
wongwh2002 <wenghongwong5686@gmail.com>
2024-11-11 20:22:01 +0800

    resolve parse double bug, resolve line number

--- src/main/java/seedu/commands/CommandUtils.java
+++ src/main/java/seedu/commands/CommandUtils.java
@@ -56,9 +56,6 @@ public class CommandUtils {
      */
     public static double roundAmount(String stringAmount, String commandFormat) {
         try {
-            if (!stringAmount.matches("\\d*(\\.\\d+)?")) {
-                throw new WiagiInvalidInputException(AMOUNT_NOT_NUMBER + commandFormat);
-            }
             double doubleAmount = Double.parseDouble(stringAmount);
             return Math.round(doubleAmount * 100.0) / 100.0; //round to 2dp
         } catch (NumberFormatException e) {
